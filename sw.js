6ea83120340db39d544c7605eda075f7195b9f2c
Pevatr <Pevatr@seznam.cz>
2026-03-04 13:54:14 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,24 +1,20 @@
-const CACHE = 'foodtrack-v2';
+const CACHE = 'foodtrack-v3';
 const FILES = ['./index.html', './manifest.json', './icon.svg'];
+
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(FILES))
-  );
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
   self.skipWaiting();
 });
+
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
   self.clients.claim();
 });
+
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    caches.match(e.request).then(cached => cached || fetch(e.request))
   );
-});
-self.addEventListener('message', e => {
-  if(e.data && e.data.type === 'SKIP_WAITING') self.skipWaiting();
 });
